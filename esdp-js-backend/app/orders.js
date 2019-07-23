cf5e4ae19662da690b6a2f9061c740cc328830bf
Aidos <aidosomurzakov@gmail.com>
2019-07-23 19:46:48 +0600

    Тестовые изменения в файле orders.js backend

--- esdp-js-backend/app/orders.js
+++ esdp-js-backend/app/orders.js
@@ -6,6 +6,9 @@ const config = require('../config');
 const nanoid = require('nanoid');
 const axios = require('axios');
 
+
+
+
 const createRouter = () => {
     router.post('/', async (req, res) => {
         const token = req.get("Authorization");
