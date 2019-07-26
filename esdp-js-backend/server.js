9536ead73532bd97f182f6a884b62a2f86c17849
greenmassa <greenmassa@gmail.com>
2019-07-26 21:49:33 +0600

    Новый кусок базы

--- esdp-js-backend/server.js
+++ esdp-js-backend/server.js
@@ -24,30 +24,99 @@
 // app.use(bodyParser.json());
 // app.use(jsonRouter({ methods: controller }));
 // app.listen(8001, () => console.log('Example app listening on port 8001'));
+const app = require('express')();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jsonRouter = require('express-json-rpc-router');
+
+const controller = {
+    testMethod({ username }) {
+        console.log('username: ', username);
+        return ['example data 1', 'example data 2']
+    },
+
+};
+
+app.use(bodyParser.json());
+app.use(jsonRouter({ methods: controller }));
+app.listen(8001, () => console.log('Example app listening on port 8001'));
 
 
 let db = {
-    get: (subject, id) => {
+    get: (subject, id, token) => {
         if (!id) {
-            return db[subject];
+            if (subject === 'orders') {
+                const user = db[subject].find((element) => {return element.token === token});
+                if (user.role === 'admin' || user.role === 'master') {
+                    return db[subject];
+                } else {
+                    return {message: "only master and admin can fetch orders"}
+                }
+            } else if (subject === 'users'){
+                if (user.role === 'admin') {
+                    return db[subject];
+                } else {
+                    return {message: "only admin can fetch users"}
+                }
+            }
+
         } else {
-            return db[subject].find((element) => {return element.id === id});
+            const item = db[subject].find((element) => {return element.id === id});
+            const user = db[subject].find((element) => {return element.token === token});
+            if (subject === 'orders') {
+                if (user.role === 'admin' || user.role === 'master') {
+                    return item;
+                } else  if (user.role === 'user' && user.id === item.client) {
+                    return item;
+                } else {
+                    return {message: "this is not your order"};
+                }
+            } else if (subject === 'users'){
+                if (user.role === 'admin' || user.role === 'courier') {
+                    return item;
+                } else {
+                    return {message: "only admin can fetch users"}
+                }
+            }
         }
     },
-    post: (subject, data) => {
+    post: (subject, data, token) => {
         if (data) {
-            data.date = new Date();
-            db[subject].push(data);
+            if (subject === 'orders') {
+                const user = db[subject].find((element) => {return element.token === token});
+                if (user.role === 'admin' || user.role === 'user') {
+                    data.date = new Date();
+                    db[subject].push(data);
+                } else {
+                    return {message: "only user and admin can create orders"}
+                }
+            } else if (subject === 'users') {
+                db[subject].push(data);
+            } else if (subject === 'cleaningTypes') {
+                data.date = new Date();
+            }
         }
     },
-    delete: (subject, id) => {
+    delete: (subject, id, token) => {
         if (id) {
-            const index = db[subject].findIndex( (element) => {return element.id === id});
-            db[subject].slice(index, 1);
+            const user = db[subject].find((element) => {return element.token === token});
+            if (user.role === 'admin') {
+                const index = db[subject].findIndex( (element) => {return element.id === id});
+                db[subject].slice(index, 1);
+            } else {
+                return {message: "you dont have enough rights to delete this"}
+            }
         }
     },
     getOrdersByStatus: (status) => {
-        return db.orders.find((element) => {return element.status === status});
+        const token;
+        const user = db[subject].find((element) => {return element.token === token});
+        if (user.role === 'master' || user.role === 'admin') {
+            return db.orders.find((element) => {return element.status === status});
+        } else {
+            return {message: "you dont have enough rights to fetch orders by status"}
+        }
+
     },
     getUsersByRole: (role) => {
         return db.users.find((user) => { return user.role === role});
