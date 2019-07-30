e17eff0ee34d77cd75bacc00af02ad8fdc76f702
greenmassa <greenmassa@gmail.com>
2019-07-30 20:12:40 +0600

    Ticket #7 Переписал главную на Material UI

--- esdp-js-frontend/src/Routes.js
+++ esdp-js-frontend/src/Routes.js
@@ -3,10 +3,12 @@ import {Route, Switch} from "react-router";
 import Register from "./containers/Register";
 import Login from "./containers/Login";
 import NewOrder from "./containers/NewOrder/NewOrder";
+import Main from "./containers/Main/Main";
 
 const Routes = ({user}) => {
     return (
         <Switch>
+            <Route path="/" component={Main}/>
             <Route path="/register" component={Register}/>
             <Route path="/login" component={Login}/>
             <Route path="/new-order" component={NewOrder}/>
---
+++ esdp-js-frontend/src/containers/Main/Main.js
@@ -0,0 +1,69 @@
+import React, {Component, Fragment} from 'react';
+import Button from '@material-ui/core/Button';
+import {connect} from "react-redux";
+import {Link} from "react-router-dom";
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from "prop-types";
+import Image from '../../assets/images/bgmain.jpg'
+
+const styles = theme => ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.background.paper,
+            marginTop: theme.spacing(12),
+        },
+    },
+    main: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: `url(${Image})`,
+        height: '700px',
+        padding: 'auto',
+        color: 'white'
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2)
+    },
+    text: {
+        color: theme.palette.text.primary,
+    },
+});
+
+class Main extends Component {
+    render() {
+        const {classes} = this.props;
+        return (
+            <Fragment>
+                <div className={classes.main}>
+                    <h1>
+                        Самая быстрая химчистка кроссовок в городе
+                    </h1>
+                    <h3>Закажи химчистку прямо сейчас</h3>
+                    <Button component={Link}
+                            to="/new-order"
+                            color="primary"
+                            variant="contained"
+                            className={classes.submit}
+                    >
+                        Оформить заказ на чистку
+                    </Button>
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+Main.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => {
+    return {
+        user: state.users.user
+    };
+};
+
+export default connect(mapStateToProps, null)(withStyles(styles)(Main));
