5ae9d955d14acaaba2422f553a5d2045a7de8b50
Gennadiy <43870498+SGGen@users.noreply.github.com>
2019-09-20 21:39:41 +0600

    Обновил delete.sql

---
+++ delete.sql
@@ -0,0 +1,17 @@
+drop table "orderItems";
+
+drop table orders;
+
+drop table clients;
+
+drop table workers;
+
+drop table statuses;
+
+drop table "cleaningTypes";
+
+drop table "pickupPoints";
+
+drop view statuses_with_readonly;
+
+drop type role;
