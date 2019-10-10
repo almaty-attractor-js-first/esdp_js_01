ca4f2e1da27deeaa857d652401df0635baa6766f
Chingiz <ch.kabykenov@gmail.com>
2019-10-10 19:07:07 +0600

    Обновил тесты

--- esdp-js-testing-placeOrder/step_definitions/steps.js
+++ esdp-js-testing-placeOrder/step_definitions/steps.js
@@ -7,31 +7,38 @@ Before(() => {
 
 Given('Я нахожусь на главной странице', () => {
   I.amOnPage("/");
+  I.wait(1)
 });
 
 When('Я нажимаю на {string}', (text) => {
   I.click(text);
+  I.wait(1)
 });
 
 When('Выбираю тип чистки {string} в поле {int} {string}', (value, num) => {
   I.selectOption({id: `cleaningType${num}`}, value);
+  I.wait(1)
 });
 
 When('Я ввожу {int} в поле {int} {string}', (int, num) => {
   I.clearField({id: `cleaningQty${num}`});
   I.fillField({id: `cleaningQty${num}`}, int);
+  I.wait(1)
 });
 
 When('в поле {int} нажимаю на кнопку +1', (num) => {
   I.click(`button[data-id='${num}']`);
+  I.wait(1)
 });
 
 When('в поле {int} нажимаю на кнопку Корзина', (num) => {
   I.click(`button[data-delete-id='${num}']`);
+  I.wait(1)
 });
 
 When('Я ввожу номер телефона {string} в поле {string}', (text, inputName) => {
   I.fillField({name: inputName}, text);
+  I.wait(1)
 });
 
 When('Я ввожу {string} {string} в поле {string}', async (type, text, inputName) => {
@@ -41,11 +48,13 @@ When('Я ввожу {string} {string} в поле {string}', async (type, text, 
   } else {
     I.fillField({name: inputName}, text);
   }
+  I.wait(1)
 });
 
 When('Я нажимаю на поле {string}', (text) => {
   I.click(text);
   I.click('28'); // @TODO Заменить число на объект даты
+  I.wait(1)
 });
 
 When('Я выбираю способ доставки {string}', (buttonName) => {
@@ -116,42 +125,69 @@ Then('Я не вижу поле {int}', (num) => {
 });
 
 Given('Я нахожусь на странице редактирования статусов', () => {
-  I.amOnPage("/edit-statuses");
+  I.amOnPage("/statuses/edit");
+  I.wait(1);
 });
 
 
 When('Я копирую название статуса из поля {int}', async (num) => {
   state.statusName = await I.grabTextFrom({id: `statusName${num}`});
+  I.wait(1);
+  I.wait(1);
 });
 
 When('Я меняю местами статус {int} и статус {int}', (num1, num2) => {
   I.dragAndDrop(`//*[@id="draggable${num1}"]`, `//*[@id="draggable${num2}"]`);
+  I.wait(1);
 });
 
 When('Я вижу новое имя статуса в поле {int}', (num) => {
   I.see(state.statusName, {id: `statusName${num}`})
+  I.wait(1);
 });
 
 When('в поле {int} нажимаю на кнопку Редактировать', (num) => {
   I.click(`button[data-edit-button='${num}']`);
+  I.wait(1);
 });
 
 When('Я в поле {int} ввожу {string} в инпут {string}', (num, value) => {
   I.fillField({id: `name${num}`}, value);
+  I.wait(1);
 });
 
 When('в поле {int} нажимаю на кнопку Отменить', (num) => {
   I.click(`button[data-discard-button='${num}']`);
+  I.wait(1);
 });
 
 When('в поле {int} нажимаю на кнопку Сохранить', (num) => {
   I.click(`button[data-submit-button='${num}']`);
+  I.wait(1);
 });
 
 Then('Я вижу название статуса в поле {int}', (num) => {
   I.see(state.statusName, {id: `statusName${num}`});
+  I.wait(1);
+  I.wait(1);
 });
 
 Then('Я не вижу название статуса в поле {int}', (num) => {
   I.dontSee(state.statusName, {id: `statusName${num}`});
+});
+
+Given('Я нахожусь на странице для авторизации админа', () => {
+  I.amOnPage("/login");
+  I.wait(1)
+});
+
+When('Я ввожу пароль {string} в поле {string}', (text, inputName) => {
+  I.fillField({name: inputName}, text);
+  I.wait(1)
+});
+
+When('Я ввожу {int} в поле {int} {string}', (int, num) => {
+  I.clearField({id: `cleaningQty${num}`});
+  I.fillField({id: `cleaningQty${num}`}, int);
+  I.wait(1)
 });
